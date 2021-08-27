e6e48a37e9a15612a7bc91a6577d226d8ed34808
Elliot Nash <elliotnash@gmail.com>
2021-08-27 15:43:44 -0700

    update help

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    
+use std::env;
+
+#[actix_web::main]
+async fn main() {
+    env::set_var("RUST_LOG", "actix_web=warn,kiln_server=debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "test-account" => {},
+        "slim" => {},
+        "view" => {},
+        command => {
+            println!("\nThe command `{}` does not exist", command);
+            send_help();
+        }
+    }
+}
+
+fn send_help() {
+    println!(
+r#"
+Avaliable commands:
+    test-account:
+        usage: kiln_server test-account <email> <password>
+        description: Tests an email/password combination
+    slim:
+        usage: kiln_server slim <email> <password> [controller serial number]
+        description: Fetches basic info about a users kiln
+    view:
+        usage: kiln_server view <email> <password> [controller serial number]
+        description: Fetches all info about a users kiln
+"#);
 }
