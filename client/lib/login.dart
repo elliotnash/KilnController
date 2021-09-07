73f6494660a9ceba5b028dd7f047cf2f4ec29f2e
Elliot Nash <elliotnash@gmail.com>
2021-09-07 09:09:26 -0700

    gesture detector, unfocus on tap outside text

--- client/lib/login.dart
+++ client/lib/login.dart
@@ -19,76 +19,85 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final query = MediaQuery.of(context);
-    return Scaffold(
-      backgroundColor: theme.colorScheme.background,
-      //extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: const Text(kTitle),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        flexibleSpace: ClipRRect(
-          borderRadius:
-              const BorderRadius.vertical(bottom: Radius.circular(15)),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            child: Container(
-              color: query.platformBrightness == Brightness.light
-                  ? theme.colorScheme.primaryVariant.withAlpha(0xAA)
-                  : theme.colorScheme.surface.withAlpha(0xAA),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        backgroundColor: theme.colorScheme.background,
+        //extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text(kTitle),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          flexibleSpace: ClipRRect(
+            borderRadius:
+                const BorderRadius.vertical(bottom: Radius.circular(15)),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                color: query.platformBrightness == Brightness.light
+                    ? theme.colorScheme.primaryVariant.withAlpha(0xAA)
+                    : theme.colorScheme.surface.withAlpha(0xAA),
+              ),
             ),
           ),
         ),
-      ),
-      //extendBody: true,
-      drawerScrimColor: Colors.transparent,
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 500),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text("Log In"),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: KilnTextField(
-                    label: kEmailLabel,
-                    hint: kEmailHint,
-                    type: TextFieldType.email,
-                    autofocus: true,
+        //extendBody: true,
+        drawerScrimColor: Colors.transparent,
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              constraints: const BoxConstraints(maxWidth: 500),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text("Log In"),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: KilnTextField(
+                      label: kEmailLabel,
+                      hint: kEmailHint,
+                      type: TextFieldType.email,
+                      autofocus: true,
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: KilnTextField(
-                    label: kPasswordLabel,
-                    hint: kPasswordHint,
-                    type: TextFieldType.password,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: KilnTextField(
+                      label: kPasswordLabel,
+                      hint: kPasswordHint,
+                      type: TextFieldType.password,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: OutlinedButton(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<OutlinedBorder>(
-                        const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    child: OutlinedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
                           ),
                         ),
                       ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        child: Text(kSubmit),
+                      ),
+                      onPressed: () {
+                        print("oh yes you did it");
+                      },
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: Text(kSubmit),
-                    ),
-                    onPressed: () {
-                      print("oh yes you did it");
-                    },
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
